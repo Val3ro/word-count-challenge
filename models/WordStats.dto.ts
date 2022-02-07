740263f7650ecf09cd3d31618becbbb7dd6be681
valeero <sergivsalvo@gmail.com>
2022-02-08 00:15:48 +0100

    removed validation decorators

--- models/WordStats.dto.ts
+++ models/WordStats.dto.ts
@@ -1,16 +1,6 @@
-import { IsNotEmpty, IsNumber, IsString } from 'class-validator';
-
 export class WordStatsDto {
-	@IsNotEmpty()
-	@IsNumber()
 	id: number;
-
-	@IsNotEmpty()
-	@IsString()
 	word: string;
-
-	@IsNotEmpty()
-	@IsNumber()
 	count: number;
 
 	constructor(wordStats: WordStatsDto) {
